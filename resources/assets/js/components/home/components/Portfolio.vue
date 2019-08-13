6ef1a4ec8e6829ff723412cd8e7e32b871638aa4
Ali Sharafi <a.sharafi1993@gmail.com>
2019-08-14 00:11:18 +0430

    Add home & components

---
+++ resources/assets/js/components/home/components/Portfolio.vue
@@ -0,0 +1,155 @@
+<template>
+  <div id="portfolio">
+    <div class="container wow fadeInUp">
+      <div class="section-header">
+        <h3 class="section-title">Portfolio</h3>
+        <p
+          class="section-description"
+        >Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <ul id="portfolio-flters">
+            <li
+              data-filter=".filter-app, .filter-card, .filter-logo, .filter-web"
+              class="filter-active"
+            >All</li>
+            <li data-filter=".filter-app">App</li>
+            <li data-filter=".filter-card">Card</li>
+            <li data-filter=".filter-logo">Logo</li>
+            <li data-filter=".filter-web">Web</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="row" id="portfolio-wrapper">
+        <div class="col-lg-3 col-md-6 portfolio-item filter-app">
+          <a href>
+            <img src="images/portfolio/app1.jpg" alt />
+            <div class="details">
+              <h4>App 1</h4>
+              <span>Alored dono par</span>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-6 portfolio-item filter-web">
+          <a href>
+            <img src="images/portfolio/web2.jpg" alt />
+            <div class="details">
+              <h4>Web 2</h4>
+              <span>Alored dono par</span>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-6 portfolio-item filter-app">
+          <a href>
+            <img src="images/portfolio/app3.jpg" alt />
+            <div class="details">
+              <h4>App 3</h4>
+              <span>Alored dono par</span>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-6 portfolio-item filter-card">
+          <a href>
+            <img src="images/portfolio/card1.jpg" alt />
+            <div class="details">
+              <h4>Card 1</h4>
+              <span>Alored dono par</span>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-6 portfolio-item filter-card">
+          <a href>
+            <img src="images/portfolio/card2.jpg" alt />
+            <div class="details">
+              <h4>Card 2</h4>
+              <span>Alored dono par</span>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-6 portfolio-item filter-web">
+          <a href>
+            <img src="images/portfolio/web3.jpg" alt />
+            <div class="details">
+              <h4>Web 3</h4>
+              <span>Alored dono par</span>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-6 portfolio-item filter-card">
+          <a href>
+            <img src="images/portfolio/card3.jpg" alt />
+            <div class="details">
+              <h4>Card 3</h4>
+              <span>Alored dono par</span>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-6 portfolio-item filter-app">
+          <a href>
+            <img src="images/portfolio/app2.jpg" alt />
+            <div class="details">
+              <h4>App 2</h4>
+              <span>Alored dono par</span>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-6 portfolio-item filter-logo">
+          <a href>
+            <img src="images/portfolio/logo1.jpg" alt />
+            <div class="details">
+              <h4>Logo 1</h4>
+              <span>Alored dono par</span>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-6 portfolio-item filter-logo">
+          <a href>
+            <img src="images/portfolio/logo3.jpg" alt />
+            <div class="details">
+              <h4>Logo 3</h4>
+              <span>Alored dono par</span>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-6 portfolio-item filter-web">
+          <a href>
+            <img src="images/portfolio/web1.jpg" alt />
+            <div class="details">
+              <h4>Web 1</h4>
+              <span>Alored dono par</span>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-6 portfolio-item filter-logo">
+          <a href>
+            <img src="images/portfolio/logo2.jpg" alt />
+            <div class="details">
+              <h4>Logo 2</h4>
+              <span>Alored dono par</span>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
