e3fb88762644545e20ede7019b110ba24b3f08dc
Ali Sharafi <a.sharafi1993@gmail.com>
2019-08-19 19:13:03 +0430

    Fix:fixed a bug

--- resources/assets/js/components/home/Home.vue
+++ resources/assets/js/components/home/Home.vue
@@ -35,7 +35,9 @@
       Contact Section
     ============================-->
     <contact />
-      <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+    <a href="#" class="back-to-top">
+      <i class="fa fa-chevron-up"></i>
+    </a>
   </div>
 </template>
 
@@ -47,36 +49,10 @@ import Action from "./components/Action";
 import Portfolio from "./components/Portfolio";
 import Team from "./components/Team";
 import Contact from "./components/Contact";
-import scopeCollection from '../../scopeCollection';
 export default {
-  components: { About, Facts, Services, Action, Portfolio, Team,Contact },
-  data(){
-      return{
-          items:[
-              {
-                  name:'ali',
-                  id:1
-              },
-              {
-                  name:'hassan',
-                  id:2
-              },
-              {
-                  name:'ali',
-                  id:3
-              }
-          ]
-      }
-  },
-  mounted(){
-      this.scopeValue();
-  },
-  methods:{
-      scopeValue(){
-          const scope = new scopeCollection(this.items);
-         let collects = scope.scopeWhere('name','ali').scopeWhere('id',3).toArray();
-        console.log(collects);
-      }
+  components: { About, Facts, Services, Action, Portfolio, Team, Contact },
+  data() {
+    return {};
   }
 };
 </script>
