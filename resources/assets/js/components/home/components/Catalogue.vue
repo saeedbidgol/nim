4969e06054a844fa66c00bfd1c3a239792c1bfa2
saeedbidgol <saeed.bidgol@gmail.com>
2019-09-13 17:41:44 +0430

    catalogue

--- resources/assets/js/components/home/components/Catalogue.vue
+++ resources/assets/js/components/home/components/Catalogue.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="catalogue">
+    <div id="right-lable">
+      <img src="uploads/background-svg/catalogue-right-lable.svg" alt="فرش">
+    </div>
+    <div id="left-lable">
+      <img src="uploads/background-svg/4.svg" alt="کاتالوگ فرش">
+    </div>
     <div class="container wow fadeIn">
       <div class="row">
-        <div class="col-lg-9 text-center text-lg-left">
-          <h3 class="cta-title">Call To Action</h3>
-          <p
-            class="cta-text"
-          >Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="col-lg-8 text-center text-lg-left">
+          <h3 class="cta-title">کاتالوگ محصولات</h3>
+          <p class="cta-text">
+            کاتالوگ محصولات شرکت فرش نیم‌رخ بافت در فرمت PDF
+          </p>
         </div>
-        <div class="col-lg-3 cta-btn-container text-center">
-          <a class="cta-btn align-middle" href="#">Call To Action</a>
+        <div class="col-lg-4 cta-btn-container text-center">
+          <a id="btn-catalogue-download" href="#">دانلود کاتالوگ</a>
         </div>
       </div>
     </div>
