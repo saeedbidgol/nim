3e4e4f286a0ba46d6ffed4ff74c00794cc7fb734
Ali Sharafi <a.sharafi1993@gmail.com>
2019-10-20 20:52:52 +0330

    change top

--- resources/assets/js/components/menus/dashboard/Right.vue
+++ resources/assets/js/components/menus/dashboard/Right.vue
@@ -1,8 +1,4 @@
 <template>
-  <!--<div class="side">
-      
-  </div>!-->
-
   <div class="page-sidebar">
     <a class="logo-box bgc85" href="/">
       <div class="logo-content">
@@ -18,99 +14,46 @@
           </router-link>
         </li>
         <li>
+          <router-link to="/dashboard">
+            <i class="menu-icon fas fa-home"></i>
+            <span>گالری محصولات</span>
+          </router-link>
+        </li>
+        <li>
           <router-link to="/repair-requests">
             <i class="menu-icon fas fa-tasks"></i>
-            <span>مشاهده درخواست ها</span>
+            <span>اسلایدر</span>
           </router-link>
         </li>
         <li>
           <router-link to="/users/profile">
             <i class="menu-icon fas fa-user-edit"></i>
-            <span>ویرایش پروفایل</span>
+            <span>درباره ما</span>
           </router-link>
         </li>
         <li>
-          <a href="javascript:void(0);">
-            <i class="menu-icon fas fa-cogs"></i>
-            <span>تنظیمات صفحه اصلی</span>
-            <i class="accordion-icon fa fa-angle-left"></i>
-          </a>
-          <ul>
-            <li>
-              <router-link to="/slides">
-                <span>اسلایدر</span>
-              </router-link>
-            </li>
-            <li>
-              <router-link to="/about-management">
-                <span>درباره مدیریت</span>
-              </router-link>
-            </li>
-            <li>
-              <router-link to="/portfolio">
-                <span>نمونه کارها</span>
-              </router-link>
-            </li>
-            <li>
-              <router-link to="/contact-us">
-                <span>اطلاعات تماس با ما</span>
-              </router-link>
-            </li>
-          </ul>
+          <router-link to="/users/profile">
+            <i class="menu-icon fas fa-user-edit"></i>
+            <span>کاتالوگ</span>
+          </router-link>
         </li>
         <li>
-          <a href="javascript:void(0);">
-            <i class="menu-icon fas fa-tools"></i>
-            <span>تعمیرکاران</span>
-            <i class="accordion-icon fa fa-angle-left"></i>
-          </a>
-          <ul>
-            <li>
-              <router-link to="service-men/management">مدیریت تعمیرکاران</router-link>
-            </li>
-            <li>
-              <a href="#">مشاهده لیست تعمیرکاران</a>
-            </li>
-            <li>
-              <a href="#">گزارش کارکرد تعمیرکاران</a>
-            </li>
-          </ul>
+          <router-link to="/users/profile">
+            <i class="menu-icon fas fa-user-edit"></i>
+            <span>صندوق پیام</span>
+          </router-link>
         </li>
         <li>
-          <a href="javascript:void(0);">
-            <i class="menu-icon fas fa-user-cog"></i>
-            <span>مدیریت کاربران</span>
-            <i class="accordion-icon fa fa-angle-left"></i>
-          </a>
-          <ul>
-            <li>
-              <router-link to="/roles">
-                <span>مدیریت نقش</span>
-              </router-link>
-            </li>
-            <li>
-              <a href="#">مدیریت کاربر</a>
-            </li>
-          </ul>
+          <router-link to="/users/profile">
+            <i class="menu-icon fas fa-user-edit"></i>
+            <span>شبکه های اجتماعی</span>
+          </router-link>
         </li>
         <li>
-          <a href="javascript:void(0);">
-            <i class="fas fa-users-cog"></i>
-            <span>مدیریت گروه های کاری</span>
-            <i class="accordion-icon fa fa-angle-left"></i>
-          </a>
-          <ul>
-            <li>
-              <router-link to="/group-work">
-                <span>مدیریت گروه</span>
-              </router-link>
-            </li>
-            <li>
-              <router-link to="/sub-group-work">
-                <span>مدیریت زیرگروه</span>
-              </router-link>
-            </li>
-          </ul>
+          <router-link to="/users/profile">
+            <i class="menu-icon fas fa-user-edit"></i>
+            <span>شماره های ثبت شده</span>
+          </router-link>
         </li>
       </ul>
     </div>
