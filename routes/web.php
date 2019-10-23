0613f1f2412f95165474aac3ceacc513b87112ad
Ali Sharafi <a.sharafi1993@gmail.com>
2019-10-24 00:01:20 +0330

    Add auth middlware

--- routes/web.php
+++ routes/web.php
@@ -14,37 +14,39 @@ Auth::routes();
 Route::get('/', "HomeController@index");
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['prefix' => 'products'], function () {
-    Route::get('', 'ProductController@getProducts');
-    Route::post('', 'ProductController@addProduct');
-    Route::delete('{product}', 'ProductController@deleteProduct');
+Route::group(['middleware' => 'auth'], function () {
+    Route::group(['prefix' => 'products'], function () {
+        Route::get('', 'ProductController@getProducts');
+        Route::post('', 'ProductController@addProduct');
+        Route::delete('{product}', 'ProductController@deleteProduct');
+    });
+
+    Route::group(['prefix' => 'site-features'], function () {
+        Route::get('about-us', 'SiteFeaturesController@getAboutUs');
+        Route::patch('about-us', 'SiteFeaturesController@updateAboutUs');
+
+        Route::group(['prefix' => 'slides'], function () {
+            Route::get('', 'SiteFeaturesController@getSlides');
+            Route::delete('{slide}', 'SiteFeaturesController@deleteSlide');
+            Route::post('', 'SiteFeaturesController@addSlide');
+            Route::post('{slide}/update', 'SiteFeaturesController@updateSlide');
+        });
+
+        Route::group(['prefix' => 'catalogues'], function () {
+            Route::get('', 'SiteFeaturesController@getCatalogues');
+            Route::delete('{catalogue}', 'SiteFeaturesController@deleteCatalogue');
+            Route::post('', 'SiteFeaturesController@addCatalogue');
+            Route::post('{catalogue}/update', 'SiteFeaturesController@updateCatalogue');
+        });
+
+        Route::group(['prefix' => 'contact-us'], function () {
+            Route::get('', 'SiteFeaturesController@getContactUs');
+            Route::patch('', 'SiteFeaturesController@updateContactUs');
+        });
+    });
+
+
+    Route::get('/dashboard', 'HomeController@dashboard');
+    Route::get('/dashboard/{any}', 'HomeController@dashboard');
+    Route::get('/{any}', 'HomeController@spa')->where('any', '.*');
 });
-
-Route::group(['prefix' => 'site-features'], function () {
-    Route::get('about-us', 'SiteFeaturesController@getAboutUs');
-    Route::patch('about-us', 'SiteFeaturesController@updateAboutUs');
-
-    Route::group(['prefix' => 'slides'], function () {
-        Route::get('', 'SiteFeaturesController@getSlides');
-        Route::delete('{slide}', 'SiteFeaturesController@deleteSlide');
-        Route::post('', 'SiteFeaturesController@addSlide');
-        Route::post('{slide}/update', 'SiteFeaturesController@updateSlide');
-    });
-
-    Route::group(['prefix' => 'catalogues'], function () {
-        Route::get('', 'SiteFeaturesController@getCatalogues');
-        Route::delete('{catalogue}', 'SiteFeaturesController@deleteCatalogue');
-        Route::post('', 'SiteFeaturesController@addCatalogue');
-        Route::post('{catalogue}/update', 'SiteFeaturesController@updateCatalogue');
-    });
-
-    Route::group(['prefix' => 'contact-us'], function () {
-        Route::get('', 'SiteFeaturesController@getContactUs');
-        Route::patch('', 'SiteFeaturesController@updateContactUs');
-    });
-});
-
-
-Route::get('/dashboard', 'HomeController@dashboard');
-Route::get('/dashboard/{any}', 'HomeController@dashboard');
-Route::get('/{any}', 'HomeController@spa')->where('any', '.*');
