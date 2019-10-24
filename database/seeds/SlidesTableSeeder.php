ad4cc34eaea1ed795c19c9ca8252888a44f7c9db
Ali Sharafi <a.sharafi1993@gmail.com>
2019-10-24 19:00:53 +0330

    Add slides table seeder

---
+++ database/seeds/SlidesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SlidesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('slides')->insert([
+            [
+                'file_url' => "https://images.unsplash.com/photo-1506260408121-e353d10b87c7?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+            ],
+            [
+                'file_url' => "https://images.unsplash.com/photo-1523712999610-f77fbcfc3843?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+            ],
+        ]);
+    }
+}
