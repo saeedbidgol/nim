c0f5a4e0bc37c952680c506ef3efb5b93b296594
Ali Sharafi <a.sharafi1993@gmail.com>
2019-10-25 11:30:00 +0330

    Ad v-select

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -11,12 +11,14 @@ import persianJs from "persianjs/persian"
 import "./veeValidate"
 import VueIziToast from "vue-izitoast";
 import {can} from './directives';
+import vSelect from 'vue-select'
 
 Vue.use(VueRouter);
 Vue.use(EasySlider);
 Vue.component("modal", Modal);
 Vue.component("loading", Loading);
 Vue.use(VueIziToast);
+Vue.component('v-select', vSelect)
 Vue.mixin(mixin);
 Vue.prototype.$user = window.user;
 const app = new Vue({
